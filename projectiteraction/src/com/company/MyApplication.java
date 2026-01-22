771055469fa43850eb0a5aebbcb741c85a0b4868
AzharSailaubek <254664@astanait.edu.kz>
2026-01-22 14:27:36 +0500

    Initial commit

--- projectiteraction/src/com/company/MyApplication.java
+++ projectiteraction/src/com/company/MyApplication.java
@@ -1,77 +1,40 @@
 package com.company;
 
-import com.company.controllers.interfaces.IUserController;
+import com.company.controllers.interfaces.IPharmacyController;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MyApplication {
+    private final IPhatrmacyController controller;
     private final Scanner scanner = new Scanner(System.in);
 
-    private final IUserController controller;
-
-    public MyApplication(IUserController controller) {
+    public MyApplication(IPharmacyController controller) {
         this.controller = controller;
     }
 
-    private void mainMenu() {
-        System.out.println();
-        System.out.println("Welcome to My Application");
-        System.out.println("Select option:");
-        System.out.println("1. Get all users");
-        System.out.println("2. Get user by id");
-        System.out.println("3. Create user");
-        System.out.println("0. Exit");
-        System.out.println();
-        System.out.print("Enter option (1-3): ");
+    private void menu() {
+        System.out.println("""
+                1. Add medicine
+                2. Show all medicines
+                3. Sell medicine
+                0. Exit
+                """);
+        System.out.print("Choose option: ");
     }
 
     public void start() {
         while (true) {
-            mainMenu();
-            try {
-                int option = scanner.nextInt();
+            menu();
+            int option = scanner.nextInt();
 
-                switch (option) {
-                    case 1: getAllUsersMenu(); break;
-                    case 2: getUserByIdMenu(); break;
-                    case 3: createUserMenu(); break;
-                    default: return;
+            switch (option) {
+                case 1 -> addMedicineMenu();
+                case 2 -> System.out.println(controller.showAllMedicines());
+                case 3 -> sellMedicineMenu();
+                default -> {
+                    return;
                 }
-            } catch (InputMismatchException e) {
-                System.out.println("Input must be integer: " + e);
-                scanner.nextLine(); // to ignore incorrect input
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
             }
-
-            System.out.println("*************************");
         }
     }
-
-    public void getAllUsersMenu() {
-        String response = controller.getAllUsers();
-        System.out.println(response);
-    }
-
-    public void getUserByIdMenu() {
-        System.out.println("Please enter id");
-
-        int id = scanner.nextInt();
-
-        String response = controller.getUser(id);
-        System.out.println(response);
-    }
-
-    public void createUserMenu() {
-        System.out.println("Please enter name");
-        String name = scanner.next();
-        System.out.println("Please enter surname");
-        String surname = scanner.next();
-        System.out.println("Please enter gender (male/female)");
-        String gender = scanner.next();
-
-        String response = controller.createUser(name, surname, gender);
-        System.out.println(response);
-    }
-}
+    
