d63688f14415ecdfd295f9d81785e022e532276f
AzharSailaubek <254664@astanait.edu.kz>
2026-01-22 14:10:06 +0500

    Initial commit

--- projectiteraction/src/com/company/Main.java
+++ projectiteraction/src/com/company/Main.java
@@ -1,25 +1,27 @@
 package com.company;
 
+import com.company.controllers.PharmacyController;
+import com.company.controllers.interfaces.IPharmacyController;
 import com.company.data.PostgresDB;
 import com.company.data.interfaces.IDB;
 import com.company.repositories.MedicineRepository;
 import com.company.repositories.SaleRepository;
+import com.company.repositories.interfaces.IMedicineRepository;
+import com.company.repositories.interfaces.ISaleRepository;
 
 public class Main {
-
     public static void main(String[] args) {
+        IDB db = new PostgresDB(
         String url = System.getenv("DB_URL");
         String user = System.getenv("DB_USER");
 
         String password = System.getenv("DB_PASSWORD");
         String dbName = System.getenv("DB_NAME");
 
-        IDB db = new PostgresDB(url, user, password, dbName);
         IUserRepository repo = new UserRepository(db);
         IUserController controller = new UserController(repo);
 
         MyApplication app = new MyApplication(controller);
-
         app.start();
 
         db.close();
