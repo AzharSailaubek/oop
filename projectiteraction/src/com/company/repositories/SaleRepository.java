dbe87c00e84c077f89ca63df0a7b5fee17ff3abf
pusechka228 <vitamin.za25.08@gmail.com>
2026-01-22 15:36:29 +0500

    initial commit

--- projectiteraction/src/com/company/repositories/SaleRepository.java
+++ projectiteraction/src/com/company/repositories/SaleRepository.java
@@ -7,3 +7,16 @@ import com.company.repositories.interfaces.ISaleRepository;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+public class SaleRepository implements ISaleRepository {
+    private final IDB db;
+
+    public SaleRepository(IDB db) {
+        this.db = db;
+    }
+
+    @Override
+    public boolean createSale(Sale s) {
+        try (Connection con = db.getConnection()) {
+            String sql = "INSERT INTO sales(medicine_id, quantity, sale_date) VALUES (?, ?, ?)";
+            PreparedStatement st = con.prepareStatement(sql);
