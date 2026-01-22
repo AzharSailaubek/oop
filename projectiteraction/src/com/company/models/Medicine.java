4326c4c67fca96d1f29552dfd19cf3efcec34262
imanbaevameruert08-prog <imanbaeva.meruert08@gmail.com>
2026-01-22 14:41:05 +0500

    initial commit

--- projectiteraction/src/com/company/models/Medicine.java
+++ projectiteraction/src/com/company/models/Medicine.java
@@ -17,5 +17,32 @@ public class Medicine {
         this.quantity = quantity;
         this.requiresPrescription = requiresPrescription;
     }
+    public Medicine(int id, String name, double price, int quantity, boolean requiresPrescription) {
+        this(name, price, quantity, requiresPrescription);
+        this.id = id;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public boolean isRequiresPrescription() {
+        return requiresPrescription;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
